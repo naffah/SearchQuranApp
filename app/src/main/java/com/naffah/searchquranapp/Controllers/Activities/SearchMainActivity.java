6d2c41c1b71253322bd7cb80e180844d660fa824
sherloxk <48653764+sherloxk@users.noreply.github.com>
2019-07-17 00:10:48 -0700

    Update SearchMainActivity.java

--- app/src/main/java/com/naffah/searchquranapp/Controllers/Activities/SearchMainActivity.java
+++ app/src/main/java/com/naffah/searchquranapp/Controllers/Activities/SearchMainActivity.java
@@ -8,6 +8,8 @@ import android.widget.Button;
 import android.widget.ImageButton;
 
 import com.naffah.searchquranapp.Controllers.Activities.IndexSearch.SearchByIndexActivity;
+import com.naffah.searchquranapp.Controllers.Activities.SpeechToText.SpeechToTextActivity;
+import com.naffah.searchquranapp.Controllers.Activities.WordSearch.SearchByArabicWordActivity;
 import com.naffah.searchquranapp.R;
 
 public class SearchMainActivity extends AppCompatActivity {
@@ -23,6 +25,24 @@ public class SearchMainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent gotoSearchByVerse = new Intent(getApplicationContext(), SearchByIndexActivity.class);
                 startActivity(gotoSearchByVerse);
+            }
+        });
+
+        Button searchByArabicWord = (Button) findViewById(R.id.searchByArabicBtn);
+        searchByArabicWord.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gotoSearchByArabic = new Intent(getApplicationContext(), SearchByArabicWordActivity.class);
+                startActivity(gotoSearchByArabic);
+            }
+        });
+
+        Button searchByVoice = (Button) findViewById(R.id.searchByVoiceBtn);
+        searchByVoice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gotoSearchByVoice = new Intent(getApplicationContext(), SpeechToTextActivity.class);
+                startActivity(gotoSearchByVoice);
             }
         });
     }
