e7082bd2cbc9a03a8e48a98c8bcc7cd78c3bc1f0
sherloxk <48653764+sherloxk@users.noreply.github.com>
2019-08-04 22:50:47 -0700

    Update VersesListAdapter.java

--- app/src/main/java/com/naffah/searchquranapp/Controllers/Adapters/VersesListAdapter.java
+++ app/src/main/java/com/naffah/searchquranapp/Controllers/Adapters/VersesListAdapter.java
@@ -1,6 +1,8 @@
 package com.naffah.searchquranapp.Controllers.Adapters;
 
 import android.content.Context;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.support.annotation.NonNull;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.widget.RecyclerView;
@@ -15,6 +17,7 @@ import com.naffah.searchquranapp.Models.Bookmarks;
 import com.naffah.searchquranapp.Models.BookmarksDatabase;
 import com.naffah.searchquranapp.R;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,7 +25,10 @@ import androidx.room.Room;
 
 public class VersesListAdapter extends RecyclerView.Adapter<VersesListAdapter.ViewHolder> {
 
-    public static final String TAG = "VersesListAdapter";
+    private String url = "http://everyayah.com/data/Abdullah_Basfar_32kbps/00X00Y.mp3";
+    private String initialUrl, finalUrl;
+    boolean isPlaying = false;
+    private MediaPlayer mediaPlayer;
 
     private static final String DATABASE_NAME = "bookmarks_db";
     private BookmarksDatabase bookmarksDatabase;
@@ -54,6 +60,10 @@ public class VersesListAdapter extends RecyclerView.Adapter<VersesListAdapter.Vi
     @Override
     public VersesListAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int i) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_verses_list_item, parent, false);
+
+        mediaPlayer = new MediaPlayer();
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+
         VersesListAdapter.ViewHolder holder = new VersesListAdapter.ViewHolder(view);
         return holder;
     }
@@ -66,9 +76,10 @@ public class VersesListAdapter extends RecyclerView.Adapter<VersesListAdapter.Vi
 
         final String Index = suraNum + ":" + versesNum.get(i);
 
+        viewHolder.bookmarkedBtn.setVisibility(View.GONE);
         for(int j = 0; j < alreadyBookmarked.size(); j++){
             if(Index.equals(alreadyBookmarked.get(j).getVerseIndex())){
-                viewHolder.bookmarkBtn.setVisibility(View.GONE);
+                viewHolder.bookmarkedBtn.setVisibility(View.VISIBLE);
             }
         }
 
@@ -86,7 +97,37 @@ public class VersesListAdapter extends RecyclerView.Adapter<VersesListAdapter.Vi
                         bookmarksDatabase.daoAccess ().insertSingleBookmark (bookmark);
                     }
                 }).start();
-                viewHolder.bookmarkBtn.setVisibility(View.GONE);
+                viewHolder.bookmarkedBtn.setVisibility(View.VISIBLE);
+            }
+        });
+
+        setInitialUrl();
+        viewHolder.playBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setFinalUrl(i);
+                if(!isPlaying && mediaPlayer!= null) {
+                    try {
+                        mediaPlayer.setDataSource(finalUrl);
+                        mediaPlayer.prepareAsync();
+                        mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                            @Override
+                            public void onPrepared(MediaPlayer mp) {
+                                mp.start();
+                            }
+                        });
+                        isPlaying = true;
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                isPlaying = false;
+                mp.reset();
             }
         });
     }
@@ -99,7 +140,7 @@ public class VersesListAdapter extends RecyclerView.Adapter<VersesListAdapter.Vi
     public class ViewHolder extends RecyclerView.ViewHolder {
 
         private TextView versesEn, versesArabic, versesNum;
-        private ImageButton bookmarkBtn;
+        private ImageButton bookmarkBtn, bookmarkedBtn, playBtn;
         private ConstraintLayout versesLayout;
 
         public ViewHolder(@NonNull View itemView) {
@@ -108,6 +149,8 @@ public class VersesListAdapter extends RecyclerView.Adapter<VersesListAdapter.Vi
             versesArabic = itemView.findViewById(R.id.verseTextArabic);
             versesNum = itemView.findViewById(R.id.verseNumber);
             bookmarkBtn = itemView.findViewById(R.id.bookmarkVerseBtn);
+            bookmarkedBtn = itemView.findViewById(R.id.bookmarkedBtn);
+            playBtn = itemView.findViewById(R.id.playBtn);
             versesLayout = itemView.findViewById(R.id.verseItemLayout);
         }
     }
@@ -120,5 +163,31 @@ public class VersesListAdapter extends RecyclerView.Adapter<VersesListAdapter.Vi
     @Override
     public int getItemViewType(int position) {
         return position;
+    }
+
+    private void setInitialUrl(){
+
+        if (Integer.parseInt(suraNum) < 10){
+            initialUrl = url.replace("X",suraNum);
+        }
+        else if(Integer.parseInt(suraNum) > 9 && Integer.parseInt(suraNum) < 100){
+            initialUrl = url.replace("0X",suraNum);
+        }
+        else {
+            initialUrl = url.replace("00X",suraNum);
+        }
+    }
+
+    private void setFinalUrl(int i){
+
+        if(Integer.parseInt(versesNum.get(i)) < 10) {
+            finalUrl = initialUrl.replace("Y", versesNum.get(i));
+        }
+        else if(Integer.parseInt(versesNum.get(i)) > 9 && Integer.parseInt(versesNum.get(i)) < 100) {
+            finalUrl = initialUrl.replace("0Y", versesNum.get(i));
+        }
+        else {
+            finalUrl = initialUrl.replace("00Y", versesNum.get(i));
+        }
     }
 }
