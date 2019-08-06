7b857ed49717d919992a3a0f586d7875b427de57
sherloxk <48653764+sherloxk@users.noreply.github.com>
2019-08-05 22:11:04 -0700

    Update IndexSearchResultActivity.java

--- app/src/main/java/com/naffah/searchquranapp/Controllers/Activities/IndexSearch/IndexSearchResultActivity.java
+++ app/src/main/java/com/naffah/searchquranapp/Controllers/Activities/IndexSearch/IndexSearchResultActivity.java
@@ -1,6 +1,8 @@
 package com.naffah.searchquranapp.Controllers.Activities.IndexSearch;
 
 import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,16 +13,22 @@ import com.naffah.searchquranapp.Models.Bookmarks;
 import com.naffah.searchquranapp.Models.BookmarksDatabase;
 import com.naffah.searchquranapp.R;
 
+import java.io.IOException;
 import java.util.List;
 
 import androidx.room.Room;
 
 public class IndexSearchResultActivity extends AppCompatActivity {
 
+    private String url = "http://everyayah.com/data/Abdullah_Basfar_32kbps/00X00Y.mp3";
+    private String initialUrl, finalUrl;
+    boolean isPlaying = false;
+
     private static final String DATABASE_NAME = "bookmarks_db";
     private BookmarksDatabase bookmarksDatabase;
     private List<Bookmarks> alreadyBookmarked;
-    private ImageButton bookmarkBtn, bookmarkedBtn;
+    private ImageButton bookmarkBtn, bookmarkedBtn, playBtn;
+    private MediaPlayer mediaPlayer;
     private String index;
 
     @Override
@@ -33,6 +41,7 @@ public class IndexSearchResultActivity extends AppCompatActivity {
         TextView transText = (TextView) findViewById(R.id.translationTextView);
         bookmarkBtn = (ImageButton) findViewById(R.id.bookmarkVerseBtn);
         bookmarkedBtn = (ImageButton) findViewById(R.id.bookmarkedBtn);
+        playBtn = (ImageButton) findViewById(R.id.playBtn);
 
         bookmarksDatabase = Room.databaseBuilder(getApplicationContext(), BookmarksDatabase.class, DATABASE_NAME).build();
 
@@ -81,5 +90,66 @@ public class IndexSearchResultActivity extends AppCompatActivity {
                 bookmarkedBtn.setVisibility(View.VISIBLE);
             }
         });
+
+        mediaPlayer = new MediaPlayer();
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+
+        setInitialUrl(suraNo);
+        setFinalUrl(verseNo);
+
+        playBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(!isPlaying && mediaPlayer!= null) {
+                    try {
+                        mediaPlayer.setDataSource(finalUrl);
+                        mediaPlayer.prepareAsync();
+                        mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                            @Override
+                            public void onPrepared(MediaPlayer mp) {
+                                mp.start();
+                            }
+                        });
+                        isPlaying = true;
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                isPlaying = false;
+                mp.reset();
+            }
+        });
+    }
+
+    private void setInitialUrl(String suraNum){
+
+        if (Integer.parseInt(suraNum) < 10){
+            initialUrl = url.replace("X",suraNum);
+        }
+        else if(Integer.parseInt(suraNum) > 9 && Integer.parseInt(suraNum) < 100){
+            initialUrl = url.replace("0X",suraNum);
+        }
+        else {
+            initialUrl = url.replace("00X",suraNum);
+        }
+    }
+
+    private void setFinalUrl(String verseNum){
+
+        if(Integer.parseInt(verseNum) < 10) {
+            finalUrl = initialUrl.replace("Y", verseNum);
+        }
+        else if(Integer.parseInt(verseNum) > 9 && Integer.parseInt(verseNum) < 100) {
+            finalUrl = initialUrl.replace("0Y", verseNum);
+        }
+        else {
+            finalUrl = initialUrl.replace("00Y", verseNum);
+        }
     }
 }
