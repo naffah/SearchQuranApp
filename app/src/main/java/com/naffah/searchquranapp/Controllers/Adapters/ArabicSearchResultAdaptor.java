8356f1e60f033befc3bf5d3efbeb8472604aec8b
sherloxk <48653764+sherloxk@users.noreply.github.com>
2019-08-05 22:13:09 -0700

    Update ArabicSearchResultAdaptor.java

--- app/src/main/java/com/naffah/searchquranapp/Controllers/Adapters/ArabicSearchResultAdaptor.java
+++ app/src/main/java/com/naffah/searchquranapp/Controllers/Adapters/ArabicSearchResultAdaptor.java
@@ -1,6 +1,8 @@
 package com.naffah.searchquranapp.Controllers.Adapters;
 
 import android.content.Context;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -13,12 +15,18 @@ import com.naffah.searchquranapp.Models.Bookmarks;
 import com.naffah.searchquranapp.Models.BookmarksDatabase;
 import com.naffah.searchquranapp.R;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import androidx.room.Room;
 
 public class ArabicSearchResultAdaptor extends RecyclerView.Adapter<ArabicSearchResultAdaptor.MyViewHolder> {
+
+    private String url = "http://everyayah.com/data/Abdullah_Basfar_32kbps/00X00Y.mp3";
+    private String initialUrl, finalUrl;
+    boolean isPlaying = false;
+    private MediaPlayer mediaPlayer;
 
     ArrayList<String> ayaList;
     ArrayList<String> transList;
@@ -49,6 +57,10 @@ public class ArabicSearchResultAdaptor extends RecyclerView.Adapter<ArabicSearch
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int i) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.arabic_search_list_item, parent, false);
+
+        mediaPlayer = new MediaPlayer();
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+
         MyViewHolder holder = new MyViewHolder(view);
         return holder;
     }
@@ -85,6 +97,37 @@ public class ArabicSearchResultAdaptor extends RecyclerView.Adapter<ArabicSearch
                 myViewHolder.bookmarkedBtn.setVisibility(View.VISIBLE);
             }
         });
+
+
+        myViewHolder.playBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setInitialUrl(i);
+                setFinalUrl(i);
+                if(!isPlaying && mediaPlayer!= null) {
+                    try {
+                        mediaPlayer.setDataSource(finalUrl);
+                        mediaPlayer.prepareAsync();
+                        mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                            @Override
+                            public void onPrepared(MediaPlayer mp) {
+                                mp.start();
+                            }
+                        });
+                        isPlaying = true;
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                isPlaying = false;
+                mp.reset();
+            }
+        });
     }
 
     @Override
@@ -95,7 +138,7 @@ public class ArabicSearchResultAdaptor extends RecyclerView.Adapter<ArabicSearch
     public static class MyViewHolder extends RecyclerView.ViewHolder {
 
         private TextView arabicText, engText, index;
-        private ImageButton bookmarkBtn, bookmarkedBtn;
+        private ImageButton bookmarkBtn, bookmarkedBtn, playBtn;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -104,6 +147,33 @@ public class ArabicSearchResultAdaptor extends RecyclerView.Adapter<ArabicSearch
             index = itemView.findViewById(R.id.index);
             bookmarkBtn = itemView.findViewById(R.id.bookmarkVerseBtn);
             bookmarkedBtn = itemView.findViewById(R.id.bookmarkedBtn);
+            playBtn = itemView.findViewById(R.id.playBtn);
+        }
+    }
+
+    private void setInitialUrl(int i){
+
+        if (Integer.parseInt(suraIndex.get(i)) < 10){
+            initialUrl = url.replace("X",suraIndex.get(i));
+        }
+        else if(Integer.parseInt(suraIndex.get(i)) > 9 && Integer.parseInt(suraIndex.get(i)) < 100){
+            initialUrl = url.replace("0X",suraIndex.get(i));
+        }
+        else {
+            initialUrl = url.replace("00X",suraIndex.get(i));
+        }
+    }
+
+    private void setFinalUrl(int i){
+
+        if(Integer.parseInt(ayaIndex.get(i)) < 10) {
+            finalUrl = initialUrl.replace("Y", ayaIndex.get(i));
+        }
+        else if(Integer.parseInt(ayaIndex.get(i)) > 9 && Integer.parseInt(ayaIndex.get(i)) < 100) {
+            finalUrl = initialUrl.replace("0Y", ayaIndex.get(i));
+        }
+        else {
+            finalUrl = initialUrl.replace("00Y", ayaIndex.get(i));
         }
     }
 }
