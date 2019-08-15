82f8ab600abe00dc04681829400218ca213f101d
sherloxk <48653764+sherloxk@users.noreply.github.com>
2019-08-15 03:14:58 -0700

    Add files via upload

--- app/src/main/java/com/naffah/searchquranapp/Controllers/Activities/MainActivity.java
+++ app/src/main/java/com/naffah/searchquranapp/Controllers/Activities/MainActivity.java
@@ -1,14 +1,16 @@
 package com.naffah.searchquranapp.Controllers.Activities;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 
 import com.naffah.searchquranapp.Controllers.Activities.Bookmarks.BookmarksListActivity;
-import com.naffah.searchquranapp.Controllers.Activities.IndexSearch.SearchByIndexActivity;
 import com.naffah.searchquranapp.Controllers.Activities.SuraList.SuraListScrollable;
 import com.naffah.searchquranapp.R;
 
@@ -18,6 +20,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SharedPreferences pref = getApplicationContext().getSharedPreferences("MyPref", 0);
+        SharedPreferences.Editor editor = pref.edit();
+
+        String check = pref.getString("translation",null);
+        if(check == null) {
+            editor.putString("translation", "database/english-translations/en.ahmedali.xml");
+            editor.apply();
+        }
 
         ImageButton suraList = (ImageButton) findViewById(R.id.suraListBtn);
         suraList.setOnClickListener(new View.OnClickListener() {
@@ -45,5 +56,25 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(gotoBookmarksList);
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.translationOption:
+                TranslationsDialog dialog =new TranslationsDialog(this);
+                dialog.show(getSupportFragmentManager(),"TranslationsDialog");
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
