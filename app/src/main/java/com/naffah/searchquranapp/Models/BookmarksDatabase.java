456c617043b5a4ac9df922b20d9327ce2d855f03
kid-phantom <48653764+kid-phantom@users.noreply.github.com>
2019-10-26 09:51:20 -0700

    Delete BookmarksDatabase.java

--- app/src/main/java/com/naffah/searchquranapp/Models/BookmarksDatabase.java
+++
@@ -1,9 +0,0 @@
-package com.naffah.searchquranapp.Models;
-
-import androidx.room.Database;
-import androidx.room.RoomDatabase;
-
-@Database(entities = {Bookmarks.class}, version = 1)
-public abstract class BookmarksDatabase extends RoomDatabase {
-    public abstract DaoAccess daoAccess() ;
-}
